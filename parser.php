84402d315bcd0818c14f46068febcbff5cc96ab6
phantom <623601391@qq.com>
2021-04-10 11:46:56 +0800

    chore: add output

--- parser.php
+++ parser.php
@@ -233,3 +233,42 @@ JSON
 } catch (\Exception $ex) {
     var_dump($ex->getMessage());
 }
+
+// php8 parser.php | sed 's/^/\/\/ /g'
+// output
+// array(5) {
+//   ["a"]=>
+//   int(100)
+//   ["b"]=>
+//   string(1) "c"
+//   ["c"]=>
+//   array(2) {
+//     ["d"]=>
+//     int(3)
+//     ["c"]=>
+//     array(4) {
+//       [0]=>
+//       int(1)
+//       [1]=>
+//       int(3)
+//       [2]=>
+//       int(4)
+//       [3]=>
+//       array(1) {
+//         ["d"]=>
+//         int(3)
+//       }
+//     }
+//   }
+//   ["e"]=>
+//   float(2.3)
+//   ["de"]=>
+//   array(3) {
+//     ["true"]=>
+//     bool(true)
+//     ["false"]=>
+//     bool(false)
+//     ["null"]=>
+//     NULL
+//   }
+// }
