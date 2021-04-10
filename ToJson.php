9443b37ef52d5c519faa4ec949fdcc0c7ada70fb
phantom <623601391@qq.com>
2021-04-10 13:47:50 +0800

    feat: optimize code and reformat code

--- ToJson.php
+++ ToJson.php
@@ -14,7 +14,7 @@ class ToJson
         return $this->encode($obj, $indent, $pretty);
     }
 
-    protected function encode($obj, int &$indent, bool $pretty = false)
+    protected function encodeBasicValue($obj): int|float|string|null
     {
         if (is_int($obj) || is_float($obj)) {
             return $obj;
@@ -24,44 +24,62 @@ class ToJson
             return $obj ? 'true' : 'false';
         } else if (is_null($obj)) {
             return 'null';
-        } else if (is_array($obj)) {
-            $count = count($obj);
-            if (0 === $count) {
-                return '[]';
-            }
-            $isObj = 'object';
-            $s = [];
-            $indent++;
-            foreach ($obj as $k => $value) {
-                if (is_int($k)) {
-                    $isObj = 'array';
-                    $s[] = $this->encode($value, $indent, $pretty);
-                } else {
-                    $s[] = sprintf('"%s": %s', $k, $this->encode($value, $indent, $pretty));
-                }
-            }
-            if (!$pretty) {
-                return 'object' === $isObj
-                    ? '{' . implode(', ', $s) . '}'
-                    : '[' . implode(', ', $s) . ']';
-            }
-            $prefix = $this->indent($indent);
-            $shortPrefix = $this->indent($indent - 1);
-
-            $result = 'object' === $isObj
-                ? '{' . PHP_EOL .
-                $prefix . implode(', ' . PHP_EOL . $prefix, $s) . PHP_EOL .
-                $shortPrefix . '}'
-                : '[' . PHP_EOL .
-                $prefix . implode(', ' . PHP_EOL . $prefix, $s) . PHP_EOL .
-                $shortPrefix . ']';
-            $indent--;
-            return $result;
         }
-
-        throw new \Exception('unknown struct');
+        return null;
     }
 
+    /**
+     * @param $obj
+     * @param int $indent
+     * @param bool $pretty
+     * @return float|int|string
+     * @throws Exception
+     */
+    protected function encode($obj, int &$indent, bool $pretty = false): string|float|int
+    {
+        $value = $this->encodeBasicValue($obj);
+        if (null !== $value) {
+            return $value;
+        }
+        if (!is_array($obj)) {
+            throw new \Exception('unknown struct');
+        }
+
+        $count = count($obj);
+        if (0 === $count) {
+            return '[]';
+        }
+        $isObj = 'object';
+        $s = [];
+        $indent++;
+        foreach ($obj as $k => $value) {
+            if (is_int($k)) {
+                $isObj = 'array';
+                $s[] = $this->encode($value, $indent, $pretty);
+            } else {
+                $s[] = sprintf('"%s": %s', $k, $this->encode($value, $indent, $pretty));
+            }
+        }
+        if (!$pretty) {
+            return 'object' === $isObj
+                ? '{' . implode(', ', $s) . '}'
+                : '[' . implode(', ', $s) . ']';
+        }
+        $prefix = $this->indent($indent);
+        $shortPrefix = $this->indent($indent - 1);
+        $itemStr = implode(', ' . PHP_EOL . $prefix, $s);
+
+        $result = 'object' === $isObj
+            ? '{' . PHP_EOL . $prefix . $itemStr . PHP_EOL . $shortPrefix . '}'
+            : '[' . PHP_EOL . $prefix . $itemStr . PHP_EOL . $shortPrefix . ']';
+        $indent--;
+        return $result;
+    }
+
+    /**
+     * @param int $indent
+     * @return string
+     */
     protected function indent(int $indent): string
     {
         return str_repeat("  ", $indent);
